c1372c0bbf7b512e9acc0b240365ba7fbb090934
apollodev66 <contact.suriya.jbch@gmail.com>
2024-10-04 15:22:11 +0700

    Initial commit

--- src/Calendar.js
+++ src/Calendar.js
@@ -1,12 +1,12 @@
-import React, { useEffect, useState, useCallback } from 'react';
-import { Calendar, momentLocalizer } from 'react-big-calendar';
-import moment from 'moment';
-import 'moment/locale/th'; // นำเข้าภาษาไทย
-import 'react-big-calendar/lib/css/react-big-calendar.css';
-import data from './data.json'; 
-import './MyCalendar.css'; 
+import React, { useEffect, useState, useCallback } from "react";
+import { Calendar, momentLocalizer } from "react-big-calendar";
+import moment from "moment";
+import "moment/locale/th"; // นำเข้าภาษาไทย
+import "react-big-calendar/lib/css/react-big-calendar.css";
+import data from "./data.json";
+import "./MyCalendar.css";
 
-moment.locale('th'); // ตั้งค่า moment เป็นภาษาไทย
+moment.locale("th"); // ตั้งค่า moment เป็นภาษาไทย
 
 const localizer = momentLocalizer(moment);
 
@@ -14,17 +14,18 @@ const MyCalendar = () => {
   const [events, setEvents] = useState([]);
 
   const convertToEvents = useCallback((data) => {
-    return data.map(item => ({
+    return data.map((item) => ({
       title: (
         <>
-          วาร: {item.วาร}&nbsp;
-          ค่ำ: {item.ค่ำ}&nbsp;
-          เดือนจีน: {item.เดือนจีน}&nbsp;<br />
-          ราศีปี: {item.ราศีจีน}<br />
-          ราศีเดือน: {item.ราศีเดือน}&nbsp;
-          ราศีวัน: {item.ราศีวัน}
+          วาร: {item.วาร}&nbsp; ค่ำ: {item.ค่ำ}&nbsp; เดือนจีน: {item.เดือนจีน}
+          &nbsp;
+          <br />
+          ราศีปี: {item.ราศีจีน}
+          <br />
+          ราศีเดือน: {item.ราศีเดือน}&nbsp; ราศีวัน: {item.ราศีวัน}
         </>
       ),
+      // แปลงปี ค.ศ. เป็น พ.ศ.
       start: new Date(item.ปี - 543, convertMonth(item.เดือน), item.วันที่),
       end: new Date(item.ปี - 543, convertMonth(item.เดือน), item.วันที่ + 1),
     }));
@@ -32,9 +33,18 @@ const MyCalendar = () => {
 
   const convertMonth = (monthName) => {
     const months = [
-      "มกราคม", "กุมภาพันธ์", "มีนาคม", "เมษายน",
-      "พฤษภาคม", "มิถุนายน", "กรกฎาคม", "สิงหาคม",
-      "กันยายน", "ตุลาคม", "พฤศจิกายน", "ธันวาคม"
+      "มกราคม",
+      "กุมภาพันธ์",
+      "มีนาคม",
+      "เมษายน",
+      "พฤษภาคม",
+      "มิถุนายน",
+      "กรกฎาคม",
+      "สิงหาคม",
+      "กันยายน",
+      "ตุลาคม",
+      "พฤศจิกายน",
+      "ธันวาคม",
     ];
     return months.indexOf(monthName);
   };
@@ -46,7 +56,7 @@ const MyCalendar = () => {
       date.getMonth() === today.getMonth() &&
       date.getDate() === today.getDate()
     ) {
-      return { style: { backgroundColor: '#fc7171' } }; // เปลี่ยนสีพื้นหลังวันที่ปัจจุบัน
+      return { style: { backgroundColor: "#fc7171" } }; // เปลี่ยนสีพื้นหลังวันที่ปัจจุบัน
     }
     return {};
   };
@@ -57,14 +67,14 @@ const MyCalendar = () => {
   }, [convertToEvents]);
 
   return (
-    <div style={{ height: '100vh' }}>
+    <div style={{ height: "100vh" }}>
       <Calendar
         localizer={localizer}
         events={events}
         startAccessor="start"
         endAccessor="end"
         dayPropGetter={dayPropGetter} // เพิ่มฟังก์ชันนี้เข้าไป
-        style={{ height: 700, margin: '50px' }}
+        style={{ height: 700, margin: "50px" }}
       />
     </div>
   );
