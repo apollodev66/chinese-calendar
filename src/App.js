9b0c9febe08152ac3c5b45e5492e73e08749f9df
apollodev66 <contact.suriya.jbch@gmail.com>
2024-10-04 14:50:51 +0700

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,13 @@
+// src/App.js
+import React from 'react';
+import MyCalendar from './Calendar';
+
+function App() {
+  return (
+    <div className="App">
+      <MyCalendar />
+    </div>
+  );
+}
+
+export default App;
---
+++ src/Calendar.js
@@ -0,0 +1,73 @@
+import React, { useEffect, useState, useCallback } from 'react';
+import { Calendar, momentLocalizer } from 'react-big-calendar';
+import moment from 'moment';
+import 'moment/locale/th'; // นำเข้าภาษาไทย
+import 'react-big-calendar/lib/css/react-big-calendar.css';
+import data from './data.json'; 
+import './MyCalendar.css'; 
+
+moment.locale('th'); // ตั้งค่า moment เป็นภาษาไทย
+
+const localizer = momentLocalizer(moment);
+
+const MyCalendar = () => {
+  const [events, setEvents] = useState([]);
+
+  const convertToEvents = useCallback((data) => {
+    return data.map(item => ({
+      title: (
+        <>
+          วาร: {item.วาร}&nbsp;
+          ค่ำ: {item.ค่ำ}&nbsp;
+          เดือนจีน: {item.เดือนจีน}&nbsp;<br />
+          ราศีปี: {item.ราศีจีน}<br />
+          ราศีเดือน: {item.ราศีเดือน}&nbsp;
+          ราศีวัน: {item.ราศีวัน}
+        </>
+      ),
+      start: new Date(item.ปี - 543, convertMonth(item.เดือน), item.วันที่),
+      end: new Date(item.ปี - 543, convertMonth(item.เดือน), item.วันที่ + 1),
+    }));
+  }, []);
+
+  const convertMonth = (monthName) => {
+    const months = [
+      "มกราคม", "กุมภาพันธ์", "มีนาคม", "เมษายน",
+      "พฤษภาคม", "มิถุนายน", "กรกฎาคม", "สิงหาคม",
+      "กันยายน", "ตุลาคม", "พฤศจิกายน", "ธันวาคม"
+    ];
+    return months.indexOf(monthName);
+  };
+
+  const dayPropGetter = (date) => {
+    const today = new Date();
+    if (
+      date.getFullYear() === today.getFullYear() &&
+      date.getMonth() === today.getMonth() &&
+      date.getDate() === today.getDate()
+    ) {
+      return { style: { backgroundColor: '#fc7171' } }; // เปลี่ยนสีพื้นหลังวันที่ปัจจุบัน
+    }
+    return {};
+  };
+
+  useEffect(() => {
+    const calendarEvents = convertToEvents(data);
+    setEvents(calendarEvents);
+  }, [convertToEvents]);
+
+  return (
+    <div style={{ height: '100vh' }}>
+      <Calendar
+        localizer={localizer}
+        events={events}
+        startAccessor="start"
+        endAccessor="end"
+        dayPropGetter={dayPropGetter} // เพิ่มฟังก์ชันนี้เข้าไป
+        style={{ height: 700, margin: '50px' }}
+      />
+    </div>
+  );
+};
+
+export default MyCalendar;
